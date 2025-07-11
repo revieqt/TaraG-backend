8cbbbe2a341c662a1667441ac24c3e7ca25cb8ad
revieqt <opsima.josh@gmail.com>
2025-07-11 19:31:16 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes will go here
+app.get('/', (_req, res) => {
+  res.send('TaraG Backend is Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
