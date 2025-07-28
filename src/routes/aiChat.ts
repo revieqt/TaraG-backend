786976acd9d5063d703bae6de6987470867fef3a
revieqt <opsima.josh@gmail.com>
2025-07-28 20:30:36 +0800

    Update: Temporarily removed unnecessary and not working codes

--- src/routes/aiChat.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { aiChatHandler } from '../controllers/aiChatController';
-
-const router = express.Router();
-
-router.post('/', aiChatHandler);
-
-export default router; 
