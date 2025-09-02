75d9e3f0794d3e0cc114b470db7e18f09b72b8df
revieqt <opsima.josh@gmail.com>
2025-09-03 00:26:32 +0800

    Temporary removed credentials in gitIgnore

---
+++ src/config/apiKeys.ts
@@ -0,0 +1,6 @@
+export const ORS_API_KEY = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImFlZGFkMzNiMGU1ZDRlZmRiNDMyZDdkMTY2OGU5Y2Q1IiwiaCI6Im11cm11cjY0In0=';
+export const OPENROUTER_API_KEY = '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';
+export const PAYMONGO_SECRET_KEY = 'sk_test_HMRLquGKhHpCF3yGBGW87tZp';
+
+export const FIREBASE_WEB_API_KEY= 'AIzaSyCn20TvjC98ePXmOEQiJySSq2QN2p0QuRg';
+export const FIREBASE_STORAGE_BUCKET= 'taralets-3adb8.firebasestorage.app';
